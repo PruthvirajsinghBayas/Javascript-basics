154012873b9c091658c01e37acd579a9c6532455
PruthvirajsinghBayas <pruthvirajsinghbayas2004@gmail.com>
2025-08-08 20:34:48 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+x = 10
+y = 5
+
+console.log("Addition of x and y is",x+y)
+console.log("Difference of x and y is",x-y)
+
+
+favouritemovie = "Mahavatar Narsimha"
+message = "I love to watch " +  favouritemovie
+console.log(message)
+
+
+ishungry = false
+if(ishungry){
+    console.log("Healthy khana khana hai")  // This is to be printed when true 
+}else{
+    console.log("Continue with  your work....") // This is to be printed when false
+}
+
+
+
