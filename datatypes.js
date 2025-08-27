46f0b0d40f8723431e4770cbe74b47e534a792e4
PruthvirajsinghBayas <pruthvirajsinghbayas2004@gmail.com>
2025-08-27 20:31:32 +0530

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -17,5 +17,21 @@ if(ishungry){
     console.log("Continue with  your work....") // This is to be printed when false
 }
 
+let arr=[1,2,4,5]
+
+let  num=arr.length +1
+let actul_sum=0
+
+const expected_sum=num * (num+1)/2
+
+arr.forEach(n =>{
+   actul_sum+=n
+})
+
+const missing_num = expected_sum - actul_sum 
+
+console.log(expected_sum)
+console.log(actul_sum)
+console.log("Missign No is :",missing_num)
 
 
